4450d4bb9de25bb5e71e3a34b5626e8f99fffc93
Dalia Hesham <78818677+Dalia-Hesham@users.noreply.github.com>
2025-01-24 13:04:05 +0000

    updates

--- server.py
+++ server.py
@@ -104,5 +104,13 @@ if __name__ == '__main__':
     # Initialize database
     initialize_database()
 
-    # Run the Flask server
-    socketio.run(app, host='0.0.0.0', port=5000)
+    # Define paths to your SSL certificate and key
+    cert_file = 'certificates/certificate.crt'
+    key_file = 'certificates/private.key'
+
+    # Create an SSL context
+    ssl_context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+    ssl_context.load_cert_chain(certfile=cert_file, keyfile=key_file)
+
+    # Use Werkzeug server with SSL
+    socketio.run(app, host='0.0.0.0', port=5000, ssl_context=ssl_context)
